ceca9d0e4ec88d9ce443ae2205145d3220038c8e
guiraccone <guilherme.raccone@hotmail.com>
2023-05-24 10:29:52 -0300

    Header Atualizado

--- ignite-projeto2-rea/src/App.tsx
+++ ignite-projeto2-rea/src/App.tsx
@@ -1,19 +1,18 @@
-import { Button } from "./components/Button";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-
-      <Button variant="primary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button variant="secondary" />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
-
-
---
+++ ignite-projeto2-rea/src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <h1>A</h1>
+}
