cf9789d61d4590eb7cd88edbc754178b778150df
guiraccone <guilherme.raccone@hotmail.com>
2023-05-29 15:29:08 -0300

    Estilizações das páginas completas

--- ignite-projeto2-rea/src/styles/global.ts
+++ ignite-projeto2-rea/src/styles/global.ts
@@ -7,7 +7,6 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
 
     }
-
     :focus{
         outline: 0;
         box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
@@ -16,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background-color: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-300']};
+        -webkit-font-smoothing: antialised;
     }
 
     body, input, textarea, button{
